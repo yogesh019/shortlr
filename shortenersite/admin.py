7749d45d83629203509597a38bf929b483ee6194
Yogesh Sharma <sharmay376@gmail.com>
2017-06-24 15:57:02 +0530

    created a model

--- shortenersite/admin.py
+++ shortenersite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from shortenersite.models import Urls
 
 # Register your models here.
+
+class UrlsAdmin(admin.ModelAdmin):
+    list_dispay=['short_id','httpurl','pub_date','count']
+    ordering=['-pub_date']
+
+admin.site.register(Urls,UrlsAdmin)
