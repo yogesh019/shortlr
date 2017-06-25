dbeb0e3a5c58b734cc9cba1e492055077affc593
Yogesh Sharma <sharmay376@gmail.com>
2017-06-26 00:56:17 +0530

    final changes- added css file

--- shortenersite/views.py
+++ shortenersite/views.py
@@ -4,7 +4,7 @@ from .models import Urls
 from django.http import HttpResponseRedirect,HttpResponse
 from django.conf import settings
 from django.core.context_processors import csrf
-
+import requests
 # Create your views here.
 
 def index(request):
@@ -16,19 +16,27 @@ def redirect_original(request,short_id):
     url=get_object_or_404(Urls,pk=short_id) # get object , if not found return 404 error
     url.count+=1
     url.save()
-    return HttpResponseRedirect(http.httpurl)
+    return HttpResponseRedirect(url.httpurl)
     
 
 def shorten_url(request):
     url=request.POST.get("url",'')
     if not (url==''):
+        if url[:7]=="http://" or url[:8]=="https://":
+            url=url
+        else:
+            url="http://"+url
+        try:
+            response=requests.get(url)
+        except requests.exceptions.ConnectionError:
+            return HttpResponse(json.dumps({"error1":"Invalid URL"}),content_type="application/json")             
         short_id=get_short_code()
         b=Urls(httpurl=url,short_id=short_id)
         b.save()
         response_data={}
         response_data['url']=settings.SITE_URL+"/"+short_id
         return HttpResponse(json.dumps(response_data),content_type="application/json")
-    return HttpResponse(json.dumps({"error":"error-occurs"},content_type="application/json")
+    return HttpResponse(json.dumps({"error":"error-occurs"}),content_type="application/json")
 
 
 def get_short_code():
@@ -36,7 +44,7 @@ def get_short_code():
     char=string.ascii_uppercase+string.digits+string.ascii_lowercase
     # if the randomly generate short_id is used than generate next
     while True:
-        short_id=''.join(rando.choice(char) for x in range(length))
+        short_id=''.join(random.choice(char) for x in range(length))
         try:
             temp=Urls.objects.get(pk=short_id)
         except:
--- shortlr/settings.py
+++ shortlr/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_URL="http://localhost:8000"
 # Application definition
 
 INSTALLED_APPS = [
